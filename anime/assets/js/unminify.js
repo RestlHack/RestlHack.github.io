7747d92f8df9b03d69fdd299ac0d37a3d9958818
RestlHackeds <43688394+RestlHack@users.noreply.github.com>
2020-10-23 00:37:19 +0700

    Add files via upload

---
+++ anime/assets/js/unminify.js
@@ -0,0 +1,99 @@
+{
+    "group_id": 126739616,
+    "appID": 7633201,
+    "group_image": "https://sun4-10.userapi.com/impf/c855020/v855020456/101d1a/EiheJsB47bc.jpg?size=50x0&quality=88&crop=191,0,1435,1435&sign=ba85f9d5b8fe04c20c1638b8113804d6&ava=1",
+    "interview": { "header": "Кто твоя вторая половинка?", "text": "Пройди тест и узнай кто твоя тян или кун, если бы ты был в аниме!" },
+    "questions": [
+        { "text": "", "img": "assets/img/0.png", "answer": { "type": "option", "variants": ["Парень", "Девушка"] } },
+        {
+            "0": [
+				{ "text": "", "img": "assets/img/female/0.png", "answer": { "type": "number" } },
+                { "text": "", "img": "assets/img/female/1.png", "answer": { "type": "option", "variants": ["Да", "Нет"] } },
+                { "text": "", "img": "assets/img/female/2.png", "answer": { "type": "option", "variants": ["Да", "Нет"] } },
+                { "text": "", "img": "assets/img/female/3.png", "answer": { "type": "option", "variants": ["Высокую", "Низкую"] } },
+                { "text": "", "img": "assets/img/female/4.png", "answer": { "type": "option", "variants": ["Да", "Нет"] } },
+				{ "text": "", "img": "assets/img/female/5.png", "answer": { "type": "option", "variants": ["Тамаки", "02", "Урарака", "Рем"] } }
+            ],
+            "1": [
+                { "text": "", "img": "assets/img/male/0.png", "answer": { "type": "number" } },
+                { "text": "", "img": "assets/img/male/1.png", "answer": { "type": "option", "variants": ["Да", "Нет"] } },
+                { "text": "", "img": "assets/img/male/2.png", "answer": { "type": "option", "variants": ["Да", "Нет"] } },
+                { "text": "", "img": "assets/img/male/3.png", "answer": { "type": "option", "variants": ["Высокого", "Низкого"] } },
+                { "text": "", "img": "assets/img/male/4.png", "answer": { "type": "option", "variants": ["Да", "Нет"] } },
+				{ "text": "", "img": "assets/img/male/5.png", "answer": { "type": "option", "variants": ["Виктор", "Леви", "Мидория", "Зак"] } }
+            ]
+        }
+    ],
+    "results": [
+        {
+            "text": "",
+            "gender": 1,
+            "description": "Тебе подходит Киришима Тоука, аниме - Токийский гуль",
+            "img": "assets/img/female/result/1.jpg",
+            "history_url": "assets/img/female/result/story/1.png"
+        },
+        {
+            "text": "",
+            "gender": 1,
+            "description": "Тебе подходит Челси, аниме - Убийца Акаме",
+            "img": "assets/img/female/result/2.jpg",
+            "history_url": "assets/img/female/result/story/2.png"
+        },
+        {
+            "text": "",
+            "gender": 1,
+            "description": "Тебе подходит Маки Одзэ, аниме- Пламенная бригада пожарных",
+            "img": "assets/img/female/result/3.jpg",
+            "history_url": "assets/img/female/result/story/3.png"
+        },
+        {
+            "text": "",
+            "gender": 1,
+            "description": "Тебе подходит Ики Хиёри, аниме Бездомный бог",
+            "img": "assets/img/female/result/4.jpg",
+            "history_url": "assets/img/female/result/story/4.png"
+        },
+        {
+            "text": "",
+            "gender": 1,
+            "description": "Тебе подходит Курияма Мирай, аниме За гранью",
+            "img": "assets/img/female/result/5.jpg",
+            "history_url": "assets/img/female/result/story/5.png"
+        },
+        {
+            "text": "",
+            "gender": 2,
+            "description": "Тебе подходит Нацу Драгнил, аниме - Хвост Феи",
+            "img": "assets/img/male/result/1.jpg",
+            "history_url": "assets/img/male/result/story/1.png"
+        },
+        {
+            "text": "",
+            "gender": 2,
+            "description": "Тебе подходит Томоэ, аниме - Приятно познакомиться, Бог",
+            "img": "assets/img/male/result/2.jpg",
+            "history_url": "assets/img/male/result/story/2.png"
+        },
+        {
+            "text": "",
+            "gender": 2,
+            "description": "Тебе подходит Хотаро Орэки, аниме Хоука тебе не уйти",
+            "img": "assets/img/male/result/3.jpg",
+            "history_url": "assets/img/male/result/story/3.png"
+        },
+        {
+            "text": "",
+            "gender": 2,
+            "description": "Тебе подходит Акихито Камбара, аниме За гранью",
+            "img": "assets/img/male/result/4.jpg",
+            "history_url": "assets/img/male/result/story/4.png"
+        },
+        {
+            "text": "",
+            "gender": 2,
+            "description": "Тебе подходит Юкино, аниме Бездомный бог",
+            "img": "assets/img/male/result/5.jpg",
+            "history_url": "assets/img/male/result/story/5.png"
+        }
+    ]
+}
